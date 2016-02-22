ebaf2eed8df01646f6df445d92f3b6c8bd9d1b68
Marii Yonov <myonov@gmail.com>
2016-02-22 09:10:46 +0200

    work in progress

--- models.py
+++ models.py
@@ -1,26 +1,51 @@
+from __future__ import print_function, unicode_literals
+
 import os
 import sys
+import datetime
+import inspect
 
 from peewee import *
 
 db = SqliteDatabase('scratch.db')
 
-class Post(Model):
+def track_tables():
+    tables = []
+    for cls, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isclass(obj) and issubclass(obj, BasicModel) and obj != BasicModel:
+            tables.append(obj)
+    return tables
+
+class BasicModel(Model):
+    class Meta:
+        database = db
+
+class Post(BasicModel):
     markdown = TextField()
     html = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    @property
+    def tags(self):
+        tag_ids = [t.tag_id for t in TagPost.select().where(TagPost.post_id == self.id)]
+        tags = Tag.select().where(Tag.id << tag_ids)
+        return tags
+
+class Tag(BasicModel):
+    name = CharField(max_length=64, index=True, unique=True)
+
+class TagPost(BasicModel):
+    tag_id = IntegerField(index=True)
+    post_id = IntegerField(index=True)
 
     class Meta:
-        database = db
-
-class Tag(Model):
-    name = CharField(max_length=64)
-
-    class Meta:
-        database = db
+        indexes = (
+            (('tag_id', 'post_id'), True),
+        )
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'create':
+    db.connect()
+    if len(sys.argv) > 1 and sys.argv[1] == 'create':
         if os.path.exists('scratch.db'):
             os.remove('scratch.db')
-        db.connect()
-        db.create_tables([Post, Tag])
+        db.create_tables(track_tables())
--- scratch.py
+++ scratch.py
@@ -1,25 +1,67 @@
 from flask import Flask, render_template, request, redirect
-from flask.helpers import url_for
+from flask.helpers import url_for, send_from_directory
+import markdown
 
 from models import *
 
 app = Flask(__name__)
+app.debug = True
+
+@app.template_filter('date')
+def _pretty_date(value):
+    return value.strftime('%H:%M:%S %d.%m.%Y')
 
 @app.route('/')
 @app.route('/index')
 def index():
     data = {
-        'posts': Post.select()
+        'posts': Post.select().order_by(Post.created_at.desc())
     }
-    return render_template('index.html')
+    return render_template('index.html', **data)
 
-@app.route('/edit', methods=['GET', 'POST'])
-def edit():
+@app.route('/edit', methods=['GET', 'POST'], endpoint='new-post')
+@app.route('/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit(post_id=None):
+
     if request.method == 'POST':
-        Post.create(markdown=request.form['editor'])
+        text_markdown = request.form['editor']
+        tags = request.form['tags'].split(',')
+
+        text_html = markdown.markdown(
+            text_markdown,
+            extensions=[
+                'markdown.extensions.fenced_code',
+                'markdown.extensions.tables',
+            ],
+        )
+
+        if post_id is None:
+            p = Post.create(markdown=text_markdown, html=text_html)
+        else:
+            p = Post.get(Post.id == post_id)
+            p.update(text_markdown=text_markdown, text_html=text_html)
+
+        for tag in tags:
+            t, created = Tag.get_or_create(name=tag)
+            TagPost.get_or_create(post_id=p.id, tag_id=t.id)
+
         return redirect(url_for('index'))
+
     return render_template('edit.html')
 
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    data = {
+        'new_post': True,
+        'post': {},
+    }
+    return render_template('edit.html', **data)
+
+if app.debug:
+    @app.route('/media/<path:filename>')
+    def media(filename):
+        for char in ',/': filename = filename.strip(char)
+        return send_from_directory(app.root_path + '/media', filename)
+
 if __name__ == '__main__':
-    app.debug = True
     app.run()
